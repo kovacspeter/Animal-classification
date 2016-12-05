8dca404749ee98771031fcc5d5bbac0dbe5ff2b3
Tom Akkermans <toak@quantleap.nl>
2016-12-05 21:50:34 +0100

    create code book

--- SIFT/pysift/sift.py
+++ SIFT/pysift/sift.py
@@ -7,101 +7,94 @@
 # Author: Jan van Gemert (j.c.vanGemert@uva.nl)
 #
 
-
 import numpy as np
-import scipy.spatial
-from pylab import *
+
+#import scipy.spatial
+from pylab import imread, matplotlib
 import sys
 
 import scipy.ndimage.filters as filters
 
-def computeDerivs(imName, sigma=1.0):
-    imRGB = imread(imName)
-    imHSV = matplotlib.colors.rgb_to_hsv(imRGB)
-    im = imHSV[:,:,2]
+
+def compute_derivatives(im_name, sigma=1.0):
+    im_rgb = imread(im_name)
+    im_hsv = matplotlib.colors.rgb_to_hsv(im_rgb)
+    im = im_hsv[:, :, 2]
+
     # Derivatives
-    dx = filters.gaussian_filter(im, sigma=sigma, order = [1,0]) 
-    dy = filters.gaussian_filter(im, sigma=sigma, order = [0,1]) 
+    dx = filters.gaussian_filter(im, sigma=sigma, order=[1, 0])
+    dy = filters.gaussian_filter(im, sigma=sigma, order=[0, 1])
     return dx, dy
 
 
-def gradAsHSV(imName, sigma=1.0):
-    #print 'showGradasHSV'
-
-    dx, dy = computeDerivs(imName,sigma=sigma)
-    ang = np.arctan2(dy,dx)
-    mag = sqrt( dx*dx + dy * dy )
+def grad_as_hsv(im_name, sigma=1.0):
+    dx, dy = compute_derivatives(im_name, sigma=sigma)
+    ang = np.arctan2(dy, dx)
+    mag = np.sqrt(dx * dx + dy * dy)
 
     threshold = mag.max()/2
-    mag[ mag > threshold ] = threshold
+    mag[mag > threshold] = threshold
 
     # convert to  HSV
-    hsv = np.zeros( (dx.shape[0], dx.shape[1], 3), dtype=np.float32 )
-    hsv[...,2] = 1
-    hsv[...,0] = (ang+np.pi) / (2*np.pi)
-    hsv[...,1] = mag / mag.max()
+    hsv = np.zeros((dx.shape[0], dx.shape[1], 3), dtype=np.float32)
+    hsv[..., 2] = 1
+    hsv[..., 0] = (ang + np.pi) / (2 * np.pi)
+    hsv[..., 1] = mag / mag.max()
     rgb = matplotlib.colors.hsv_to_rgb(hsv)
     return rgb
 
-def densePoints(imName, stride=5):
-    #print 'dense sampling', 
-    imRGB = imread(imName)
-    [rows,cols,clrs] = imRGB.shape
+
+def dense_points(im_name, stride=5):
+    im_rgb = imread(im_name)
+    [rows, cols, _] = im_rgb.shape
     
     # divide 1 stride on both sides
     offsetR = rows % stride
     offsetC = cols % stride
-    
-    
-    r = np.arange( offsetR/2, rows, stride)
-    c = np.arange( offsetC/2, cols, stride)
-    x, y = meshgrid(c, r) 
-    grid = np.array( [x.flatten(), y.flatten()] ).T
-    #print 'nr Points', grid.shape[0]
+
+    r = np.arange(offsetR/2, rows, stride)
+    c = np.arange(offsetC/2, cols, stride)
+    x, y = np.meshgrid(c, r)
+    grid = np.array([x.flatten(), y.flatten()]).T
     return grid
 
 
-def computeHes(imName, sigma=1, magThreshold = 10, hesThreshold=5, NMSneighborhood = 10):
-    #print 'hessian'
-    imRGB = imread(imName)
+def compute_hes(im_name, sigma=1, magThreshold = 10, hesThreshold=5, NMSneighborhood = 10):
+    im_rgb = imread(im_name)
     
     # convert to grayscale
-    imHSV = matplotlib.colors.rgb_to_hsv(imRGB)
-    im = imHSV[:,:,2]
+    im_hsv = matplotlib.colors.rgb_to_hsv(im_rgb)
+    im = im_hsv[:, :, 2]
     
     # Derivatives
-    dxx = filters.gaussian_filter(im, sigma=sigma, order = [2,0])
-    dyy = filters.gaussian_filter(im, sigma=sigma, order = [0,2])
+    dxx = filters.gaussian_filter(im, sigma=sigma, order=[2, 0])
+    dyy = filters.gaussian_filter(im, sigma=sigma, order=[0, 2])
     lapl = sigma * (dxx + dyy)
 
     # non max suppression and thresholding of maxima
     data_max = filters.maximum_filter(lapl, NMSneighborhood)
     maxima = (lapl == data_max)
-    maxima = logical_and( maxima, data_max > hesThreshold)
+    maxima = np.logical_and(maxima, data_max > hesThreshold)
 
     # non max suppression and thresholding of minima
     data_min = filters.minimum_filter(lapl, NMSneighborhood)
     minima = (lapl == data_min)
-    minima = logical_and( minima, data_min < -hesThreshold)
+    minima = np.logical_and(minima, data_min < -hesThreshold)
 
-    extrema = logical_or(maxima, minima)
+    extrema = np.logical_or(maxima, minima)
 
-    dx = filters.gaussian_filter(im, sigma=sigma, order = [1,0])
-    dy = filters.gaussian_filter(im, sigma=sigma, order = [0,1])
-    mag = sigma * sqrt( dx*dx + dy * dy )
-    extrema = logical_and( extrema, mag > magThreshold)
+    dx = filters.gaussian_filter(im, sigma=sigma, order=[1, 0])
+    dy = filters.gaussian_filter(im, sigma=sigma, order=[0, 1])
+    mag = sigma * np.sqrt( dx*dx + dy * dy)
+    extrema = np.logical_and( extrema, mag > magThreshold)
 
-    #print 'Hes, nr Points', sum(extrema)
+    [r, c] = np.where(extrema)
 
-    [r,c] = np.where(extrema)
-
-    return np.array([c,r]).T
+    return np.array([c, r]).T
 
 
-def computeHar(imName, sigma=1, k=0.04, magThreshold = 10, NMSneighborhood=10):
-    #print 'harris'
-    
-    dx, dy = computeDerivs(imName,sigma=sigma)
+def computeHar(imName, sigma=1, k=0.04, magThreshold=10, NMSneighborhood=10):
+    dx, dy = compute_derivatives(imName, sigma=sigma)
 
     #structure tensor
     dx2 = dx * dx
@@ -109,7 +102,7 @@ def computeHar(imName, sigma=1, k=0.04, magThreshold = 10, NMSneighborhood=10):
     dxy = dx * dy
 
     # average with integration scale
-    iScale=2*sigma
+    iScale = 2*sigma
     dx2 = filters.gaussian_filter(dx2, sigma=iScale)
     dy2 = filters.gaussian_filter(dy2, sigma=iScale)
     dxy = filters.gaussian_filter(dxy, sigma=iScale)
@@ -120,25 +113,21 @@ def computeHar(imName, sigma=1, k=0.04, magThreshold = 10, NMSneighborhood=10):
     # non-maximum suppression 
     data_max = filters.maximum_filter(R, NMSneighborhood)
     maxima = (R == data_max)
-    maxima = logical_and( maxima, data_max > 0)
+    maxima = np.logical_and( maxima, data_max > 0)
 
     # threshold magnitude
     mag = sigma * sqrt( dx*dx + dy * dy )
-    maxima = logical_and( maxima, mag > magThreshold)
+    maxima = np.logical_and( maxima, mag > magThreshold)
 
-    #print 'Har, nr Points', sum(maxima)
     [r,c] = np.where(maxima)
 
     return np.array([c,r]).T
 
 
 def computeSIFTofPoints(imName, points, sigma=1.0, nrOrientBins=8, nrSpatBins = 4, nrPixPerBin = 4):
-    #print 'computeSIFTofPoints: %d points' % points.shape[0],
 
-    dx, dy = computeDerivs(imName,sigma=sigma)
-    #print dx.shape
+    dx, dy = compute_derivatives(imName, sigma=sigma)
     r, c = dx.shape
-    #ang = np.arctan2(dy,dx)
     mag = sqrt( dx*dx + dy * dy )
 
     # put derivative vectors in an array, and add system precision 
@@ -147,7 +136,6 @@ def computeSIFTofPoints(imName, points, sigma=1.0, nrOrientBins=8, nrSpatBins = 
     # L2 normalize so dotprod is the cosine
     derivVecs = (derivVecs.T / np.linalg.norm(derivVecs,2,axis=1).T).T
 
-    ## convert angles to vectors for easy dist-computation
     binCenters = np.arange(nrOrientBins) * (2*pi)/(nrOrientBins*2)
     binVec = np.array([ cos(binCenters), sin(binCenters) ])
 
@@ -183,36 +171,19 @@ def computeSIFTofPoints(imName, points, sigma=1.0, nrOrientBins=8, nrSpatBins = 
             cBegin = j * nrPixPerBin + nrPixPerBinDiv2
             cEnd = c - nrPixPerBin  * (nrSpatBins-j-1) - nrPixPerBinDiv2
 
-            #print rBegin, rEnd, cBegin, cEnd
             feats[:,:,idx:nrOrientBins+idx] = aggAng[rBegin:rEnd, cBegin:cEnd, :]
             idx = idx + nrOrientBins
-    #print feats[1:3,1:3,:]
-    #print feats.shape
 
-    
     pointsC, pointsR = points[:,0], points[:,1]
     inBorder = pointsR>nrPixPerBin*nrSpatBins
     inBorder = np.logical_and(inBorder, pointsR<r-nrPixPerBin*nrSpatBins)
-
     inBorder = np.logical_and(inBorder, pointsC>nrPixPerBin*nrSpatBins)
     inBorder = np.logical_and(inBorder, pointsC<c-nrPixPerBin*nrSpatBins)
     pointsR = pointsR[inBorder]
     pointsC = pointsC[inBorder]
-    
-    #print pointsR.shape
-    #print pointsC.shape
-    
     pointFeat = feats[pointsR, pointsC, :]
     pointFeat = (pointFeat.T / np.linalg.norm(pointFeat + sys.float_info.epsilon,2,axis=1).T).T
-    #print pointFeat
-    #print pointFeat.shape
 
-    #print pointFeat[1:3,:]
-    #figure()
-    #imshow(pointFeat)
-    #show()
-    #print 'kept:', pointFeat.shape
-    #sys.stdout.flush()
 
     return np.array([pointsC, pointsR]).T, pointFeat
 
