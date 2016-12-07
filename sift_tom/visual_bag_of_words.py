07119d64a66f2d38a8f704537458736e0c73a211
Tom Akkermans <toak@quantleap.nl>
2016-12-07 22:08:26 +0100

    refactor: only extract features once

--- sift_tom/visual_bag_of_words.py
+++ sift_tom/visual_bag_of_words.py
@@ -1,99 +1,114 @@
 from __future__ import print_function, division
+from collections import OrderedDict
 import logging
 import time
+import os
+import pickle
 import numpy as np
 from sklearn.metrics import euclidean_distances
 from sklearn.cluster import MiniBatchKMeans, KMeans
 from sklearn.neighbors import KNeighborsClassifier
 import sift
+from hyper_pars import *
 
 logger = logging.getLogger(__file__)
-data_path = '../AnimalNet/'
+data_path = '../data/'
 
 
 def get_sift_points_from_image(image_path):
     # calculate several types of points of interest from an image
-    # HYPER PARAMETERS HERE
-    dense_points = sift.dense_points(image_path, stride=25)
-    hessian_points = sift.compute_hesssian_points(image_path, sigma=1.0, mag_threshold=15, hes_threshold=10, nms_neighborhood=10)
-    harris_points = sift.compute_harris_points(image_path, sigma=1.0, mag_threshold=5, nms_neighborhood=10)
+    image_name = os.path.basename(image_path)
+    points = []
 
-    # combine dense, harris and hessian interest points
-    points = np.concatenate((dense_points, hessian_points, harris_points))
-    logger.debug("There are {} SIFT points (dens={}, hes={}, har={}) in image {}".format(
-                len(points), dense_points.shape[0], hessian_points.shape[0], harris_points.shape[0], image_path))
-    return points
+    # dense points
+    if SIFT_USE_DENSE_POINTS:
+        dense_points = sift.dense_points(image_path, stride=SIFT_DENSE_POINT_STRIDE)  # N x 2
+        logger.debug('dense points: {} for image {}'.format(len(dense_points), image_name))
+        points.append(dense_points)
+
+    # Harris points
+    if SIFT_USE_HARRIS_POINTS:
+        harris_points = sift.compute_hesssian_points(image_path,
+                                                     sigma=SIFT_HARRIS_POINT_SIGMA,
+                                                     mag_threshold=SIFT_HARRIS_POINT_MAG_THRESHOLD,
+                                                     hes_threshold=SIFT_HARRIS_POINT_HES_THRESHOLD,
+                                                     nms_neighborhood=SIFT_HARRIS_POINT_NSM_NEIGHBORHOOD)
+        logger.debug('harris points: {} for image {}'.format(len(harris_points), image_name))
+        points.append(harris_points)
+
+    # Hessian points
+    if SIFT_USE_HESSIAN_POINTS:
+        hessian_points = sift.compute_harris_points(image_path,
+                                                    sigma=SIFT_HESSIAN_POINT_SIGMA,
+                                                    mag_threshold=SIFT_HESSIAN_POINT_MAG_THRESHOLD,
+                                                    nms_neighborhood=SIFT_HESSIAN_POINT_NSM_NEIGHBORHOOD)
+        logger.debug('hessian points: {} for image {}'.format(len(hessian_points), image_name))
+        points.append(hessian_points)
+    return np.vstack(points)
 
 
 def get_sift_features_from_image(image_path):
     # calc SIFT features from sift points - 128 dim vector per SIFT point - some SIFT points will be discarded
     points = get_sift_points_from_image(image_path)
     _, sift_features = sift.compute_sift_to_points(image_path, points,
-                                                sigma=1.0, nr_orient_bins=8, nr_spat_bins=4, nr_pix_per_bin=4)
-    logger.debug("There are {} SIFT features in image {}".format(len(sift_features), image_path))
+                                                   sigma=1.0, nr_orient_bins=8, nr_spat_bins=4, nr_pix_per_bin=4)
     return sift_features
 
 
-def get_sift_features_from_images(image_paths, name):
-    # collect all sift features from all images
-    logger.debug("no of images: {}".format(len(image_paths)))
+def get_sift_features_dict_from_images(image_paths, name):
+    # collect all sift features from all images, store in dict with image index as key
     start = time.time()
-    features_list = []
+    feature_dict = OrderedDict()  # necessary as we need to maintain order when using dict.values() unpacking
     for idx, image_path in enumerate(image_paths):
         logger.debug('process image ({}/{})'.format(idx, len(image_paths)))
         sift_features = get_sift_features_from_image(image_path)
         len(sift_features)
-        features_list.append(sift_features)
-    logger.debug("no of features in bag: {}".format(sum([len(i) for i in features_list])))
-    logger.debug("feature collection done in {} sec".format(time.time()-start))
-    logger.debug("saving features...")
-    features = np.vstack(features_list)
-    np.save('{}_features'.format(name), features)
-    return features
+        feature_dict[idx] = sift_features
+    logger.info("feature collection done in {} sec".format(time.time()-start))
+    logger.info("saving features...")
+    pickle.dump(feature_dict, open(name+'_features.dat', 'wb'))
+    return feature_dict
 
 
-def create_clusters(features, use_mini_batch=True):
-    # create clusters from feature set
-    # HYPER PARAMETERS HERE
-    n_clusters=200
+def create_code_book(features):
+    # create code_book from feature set
     logger.info("starting k-means clustering...")
     start = time.time()
-    if use_mini_batch:
-        mini_batch_k_means = MiniBatchKMeans(n_clusters=n_clusters, max_iter=3, verbose=True)  # HYPER
+    if CODE_BOOK_KMEANS_USE_MINI_BATCH:
+        mini_batch_k_means = MiniBatchKMeans(n_clusters=CODE_BOOK_KMEANS_CLUSTERS,
+                                             verbose=False)
         mini_batch_k_means.fit(features)
-        centroids = mini_batch_k_means.cluster_centers_
+        code_book = mini_batch_k_means.cluster_centers_
     else:
-        k_means = KMeans(n_clusters=200, verbose=True)  # HYPER  , n_jobs=2 breaks on MAC
+        k_means = KMeans(n_clusters=CODE_BOOK_KMEANS_CLUSTERS, verbose=False)  # n_jobs=2 breaks on MAC
         k_means.fit(features)
-        centroids = k_means.cluster_centers_
+        code_book = k_means.cluster_centers_
     logger.info("clustering done in {} sec".format(time.time()-start))
     logger.info("saving code book...")
-    np.save('clusters_{}.npy'.format(n_clusters), centroids)
-    return centroids
+    np.save('code_book.npy', code_book)
+    return code_book
 
 
 def create_histogram_from_features(features, clusters):
-    # create a histogram of assigned clusters to features
-    feature_cluster = euclidean_distances(clusters, features).argmin(axis=0)
-    histogram = np.bincount(feature_cluster, minlength=clusters.shape[0]) / features.shape[0]
+    # create a histogram of assigned code_book to features
+    if features.shape[0] != 0:
+        feature_cluster = euclidean_distances(clusters, features).argmin(axis=0)
+        histogram = np.bincount(feature_cluster, minlength=clusters.shape[0]) / features.shape[0]
+    else:
+        # return empty histogram for images without features
+        # e.g. killer+whale_0011.jpg has no Harris points
+        histogram = np.zeros((clusters.shape[0]))
     return histogram
 
 
-def create_histogram_from_image(image_path, clusters):
-    # create a feature occurrence histogram from a image
-    features = get_sift_features_from_image(image_path)
-    histogram = create_histogram_from_features(features, clusters)
-    return histogram
-
-
-def create_histograms_from_images(image_paths, clusters):
-    # create a feature occurrence histogram for each image
-    n = len(image_paths)
-    k = clusters.shape[0]
-    histograms = np.zeros((n, k))
-    for i, image_path in enumerate(image_paths):
-        logger.info('create histogram {}/{}'.format(i, len(image_paths)))
-        histograms[i] = create_histogram_from_image(image_path, clusters)
+def create_histograms(clusters, features_dict, name):
+    # create histogram from image features
+    logger.info('make {} histograms from image features...'.format(name))
+    histograms = np.zeros((len(features_dict), clusters.shape[0]))
+    for idx, key in enumerate(features_dict):
+        logger.info('create histogram {}/{}'.format(idx, len(features_dict)))
+        histograms[idx] = create_histogram_from_features(features_dict[idx], clusters)
+    np.save('{}_histograms.npy'.format(name), histograms)
     return histograms
 
 
@@ -113,18 +128,8 @@ def get_val_labels():
     return [int(line.strip().split(" ")[1]) for line in open(data_path+"valset-overview.txt", "r")]
 
 
-def create_histograms(clusters, train_images, val_images):
-    logger.info("make train image histograms...")
-    train_histograms = create_histograms_from_images(train_images, clusters)
-    np.save('train_histogram.npy', train_histograms)
-
-    logger.info("make val image histograms...")
-    val_histograms = create_histograms_from_images(val_images, clusters)
-    np.save('val_histogram.npy', val_histograms)
-
-
 def k_nearest_neighbors(train_histograms, train_labels, val_histograms):
-    knn = KNeighborsClassifier(n_neighbors=10, weights='uniform', n_jobs=2)
+    knn = KNeighborsClassifier(n_neighbors=CLASSIFIER_KNN_NEIGHBORS, weights=CLASSIFIER_KNN_WEIGHTS, n_jobs=N_JOBS)
     knn.fit(train_histograms, train_labels)
     predictions = knn.predict(val_histograms)
     return predictions
@@ -141,28 +146,37 @@ def compute_accuracy(predictions, truth):
     return (np.sum(predictions == truth)) / len(truth)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     train_images = get_train_images()
     train_labels = get_train_labels()
     val_images = get_val_images()
     val_labels = get_val_labels()
 
-    # create codebook
+    # extract features for all images
     if True:
-        if False:
-            train_features = np.load('train_features.npy')
-        else:
-            train_features = get_sift_features_from_images(train_images[:10], 'train')
-        #create_clusters(train_features)
+        train_features_dict = get_sift_features_dict_from_images(train_images, 'train')
+        val_features_dict = get_sift_features_dict_from_images(val_images, 'val')
+    else:
+        train_features_dict = pickle.load(open('train_features.dat', 'rb'))
+        val_features_dict = pickle.load(open('val_features.dat', 'rb'))
+
+    # create code book / clusters
+    if True:
+        all_train_features = np.vstack(train_features_dict.values())
+        code_book = create_code_book(all_train_features)
+    else:
+        code_book = np.load('code_book.npy')
 
     # create histograms
-    if False:
-        clusters = np.load('clusters_200.npy')
-        create_histograms(clusters, train_images, val_images)
+    if True:
+        train_histograms = create_histograms(code_book, train_features_dict, 'train')
+        val_histograms = create_histograms(code_book, val_features_dict, 'val')
+    else:
+        train_histograms = np.load('train_histogram.npy')
+        val_histograms = np.load('val_histogram.npy')
 
     # classify images
-    if False:
-        predictions = classify(np.load('train_histogram.npy'), train_labels, np.load('val_histogram.npy'), val_labels)
-        print("accuracy: {}".format(compute_accuracy(predictions, val_labels)))
+    predictions = classify(train_histograms, train_labels, val_histograms, val_labels)
+    print("accuracy: {}".format(compute_accuracy(predictions, val_labels)))
     logger.debug("DONE")
